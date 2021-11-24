634ed46c9373d77cb89a85978365bd7980fb9e48
Bill Melvin <bill@billmelvin.com>
2021-11-24 17:08:23 -0500

    Add options for '--log-dir' and '--compare-cmd'. Add log file for stdout from compare tool.

--- bak_to_git_2.py
+++ bak_to_git_2.py
@@ -15,14 +15,39 @@ from collections import namedtuple
 from datetime import datetime
 from pathlib import Path
 
+from bak_to_common import log_fmt
 
-AppOptions = namedtuple("AppOptions", "input_csv, skip_backup")
+
+AppOptions = namedtuple(
+    "AppOptions", "input_csv, skip_backup, log_dir, compare_cmd"
+)
 
 
-def run_bcompare(left_file, right_file):
+run_dt = datetime.now()
+
+log_path = Path.cwd() / f"log-bak_to_git_2-{run_dt:%Y%m%d_%H%M%S}.txt"
+
+
+def write_log(msg, do_print=False):
+    if do_print:
+        print(msg)
+    with open(log_path, "a") as log_file:
+        log_file.write(f"{msg}\n")
+
+
+def run_compare(compare_cmd, left_file, right_file):
     print(f"\nCompare\n  L: {left_file}\n  R: {right_file}\n")
 
-    result = subprocess.run(["bcompare", left_file, right_file])
+    cmds = [compare_cmd, left_file, right_file]
+
+    write_log(f"RUN: {log_fmt(cmds)}")
+
+    result = subprocess.run(
+        cmds, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+    )
+
+    if 0 < len(result.stdout):
+        write_log(f"STDOUT: {result.stdout.strip()}")
 
     assert result.returncode in [0, 1, 2, 11, 12, 13]
     #  bcompare return codes:
@@ -33,13 +58,15 @@ def run_bcompare(left_file, right_file):
     #      11  Binary differences
     #      12  Similar
     #      13  Rules-based differences
+    #
+    # TODO: Look at return codes for other tools (kdiff3, meld).
 
 
 def get_opts(argv) -> AppOptions:
 
     ap = argparse.ArgumentParser(
         description="BakToGit Step 2: Run Beyond Compare (bcompare) to "
-        + "review source file changes between 'wipbak' backups. This is"
+        + "review source file changes between 'wipbak' backups. This is "
         + "to be used along with manually editing the CSV file to set "
         + "the 'SKIP_Y' and 'COMMIT_MESSAGE' columns."
     )
@@ -54,16 +81,41 @@ def get_opts(argv) -> AppOptions:
         "--skip-backup",
         dest="skip_backup",
         action="store_true",
-        help="Do not create a backup of the input CSV file. By default a "
+        help="Do not create a backup of the input CSV file. By default, a "
         + "backup copy is created at the start of a session.",
+    )
+
+    ap.add_argument(
+        "--log-dir",
+        dest="log_dir",
+        action="store",
+        help="Output directory for log files.",
+    )
+
+    ap.add_argument(
+        "--compare-cmd",
+        dest="compare_cmd",
+        default="bcompare",
+        action="store",
+        help="Name of the executable command to launch the file comparison "
+        + "tool. The default is 'bcompare' (Beyond Compare).",
     )
 
     args = ap.parse_args(argv[1:])
 
-    opts = AppOptions(args.input_csv, args.skip_backup)
+    opts = AppOptions(
+        args.input_csv, args.skip_backup, args.log_dir, args.compare_cmd
+    )
 
     assert Path(opts.input_csv).exists()
     assert Path(opts.input_csv).is_file()
+
+    if opts.log_dir is not None:
+        if not Path(opts.log_dir).exists():
+            sys.stderr.write(
+                f"ERROR: Log directory not found '{opts.log_dir}'"
+            )
+            sys.exit(1)
 
     return opts
 
@@ -85,7 +137,7 @@ def get_rename(add_command):
         return ""
 
 
-def process_row(row, prevs):
+def process_row(compare_cmd, row, prevs):
     print(f"Row sort_key = '{row['sort_key']}'")
     base_name = row["base_name"]
     no_msg = len(row["COMMIT_MESSAGE"]) == 0
@@ -111,7 +163,7 @@ def process_row(row, prevs):
             prev_key = base_name
 
         if prev_key in prevs.keys():
-            run_bcompare(prevs[prev_key], row["full_name"])
+            run_compare(compare_cmd, prevs[prev_key], row["full_name"])
         else:
             if len(row["prev_full_name"]) == 0:
                 print(f"\nNew file: {base_name}")
@@ -120,20 +172,20 @@ def process_row(row, prevs):
             else:
                 warning = "UNEXPECTED PREVIOUS VERSION"
                 print(f"\n{warning}: {base_name}")
-                run_bcompare(
-                    row["prev_full_name"], row["full_name"]
+                run_compare(
+                    compare_cmd, row["prev_full_name"], row["full_name"]
                 )
 
-        answer = input("Continue (or Keep left) [Y,n,k]? ")
+        answer = input(
+            "(k = Keep left file for next comparison) Continue [Y,n,k]? "
+        )
 
         if answer.lower() == "n":
             print("\nStopping.\n")
             return False
 
         if answer.lower() == "k":
-            print(
-                "\nKeeping previous Left file for comparison.\n"
-            )
+            print("\nKeeping previous Left file for comparison.\n")
         else:
             prevs[base_name] = row["full_name"]
             print("")
@@ -147,6 +199,14 @@ def main(argv):
 
     opts = get_opts(argv)
 
+    global log_path
+    if opts.log_dir is not None:
+        log_path = (
+            Path(opts.log_dir).expanduser().resolve().joinpath(log_path.name)
+        )
+
+    write_log(f"BEGIN at {run_dt:%Y-%m-%d %H:%M:%S}")
+
     if not opts.skip_backup:
         #  Make a backup of the input_csv in case there are problems while
         #  manually editing the file.
@@ -155,15 +215,20 @@ def main(argv):
         bak_path = p.with_name(f"z-{p.name}").with_suffix(bak_suffix)
         assert not bak_path.exists()
         print(f"\nSaving backup as '{bak_path.name}'\n")
+        write_log(f"BACKUP: '{bak_path}'")
         shutil.copyfile(opts.input_csv, bak_path)
+
+    write_log(f"READ: '{opts.input_csv}'")
 
     prevs = {}
     with open(opts.input_csv) as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
             if len(row["sort_key"]) > 0:
-                if not process_row(row, prevs):
+                if not process_row(opts.compare_cmd, row, prevs):
                     break
+
+    write_log(f"END at {datetime.now():%Y-%m-%d %H:%M:%S}")
 
     print("Done (bak_to_git_2.py).")
 
